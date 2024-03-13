a1143330a489c99dac107f125db906d2d7e52767
XsporosX <saul_751@hotmail.com>
2024-03-12 22:57:46 -0400

    PRESENTACION WEB

--- scripts/index.js
+++ scripts/index.js
@@ -12,13 +12,13 @@ class Repository {
     this.activities = [];
     this.id = 1;
   }
-  getAllActivities() {
-    return this.activities;
-  }
   createActivity(title, description, imgURL) {
     const id = this.id++;
     const nuevaAvtividad = new Activity(id, title, description, imgURL);
     this.activities.push(nuevaAvtividad);
+  }
+  getAllActivities() {
+    return this.activities;
   }
   deleteActivity(id) {
     this.activities = this.activities.filter((activity) => activity.id !== id);
@@ -38,3 +38,41 @@ repository.deleteActivity(3);
 
 console.log("Actividades después de eliminar la actividad con ID 3: ", repository.getAllActivities());
 
+
+
+const addBtn = document.getElementById("add");
+const body = document.getElementsByTagName("body")[0];
+
+const addBtnHandler = () => {
+
+  const nuevoDiv = document.createElement("div");
+  nuevoDiv.classList.add("main__cards");
+  nuevoDiv.addEventListener("click", (event) => {
+    event.target.remove();
+  });
+
+  const title = document.getElementById("title");
+  const description = document.getElementById("description");
+  const imgUrl = document.getElementById("imgUrl");
+
+  const titleElement = document.createElement("p");
+  titleElement.textContent = title.value;
+
+  const descriptionElement = document.createElement("p");
+  descriptionElement.textContent = description.value;
+
+  const imgElement = document.createElement("img");
+  imgElement.src = imgUrl.value;
+
+  nuevoDiv.appendChild(titleElement);
+  nuevoDiv.appendChild(descriptionElement);
+  nuevoDiv.appendChild(imgElement);
+
+  title.value = "";
+  description.value = "";
+  imgUrl.value = "";
+
+  body.appendChild(nuevoDiv);
+}
+
+addBtn.addEventListener("click", addBtnHandler);
